becb0bc89062ef6728cb7501d01ccb493f87151f
Erxhan Ganiu <ganijerd@students.zhaw.ch>
2024-05-26 21:40:47 +0200

    Semi Auto Deploy

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hoi Erxhan');
+    res.send('Hoi Erxhan Semi Auto');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
