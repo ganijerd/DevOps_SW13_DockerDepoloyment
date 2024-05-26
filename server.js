8b603cb030a9b54e9796cb2ebea3234e018dec6d
Erxhan Ganiu <ganijerd@students.zhaw.ch>
2024-05-26 22:04:47 +0200

    Test22

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hoi Erxhan Semi Auto ---> ');
+    res.send('Hoi Erxhan Semi Auto ---> A');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
