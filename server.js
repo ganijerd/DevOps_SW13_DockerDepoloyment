fa422ca9165387c7517ad9b06da87c1923d3a1ea
Erxhan Ganiu <ganijerd@students.zhaw.ch>
2024-05-26 20:49:36 +0200

    Test Demo

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
     res.send('Hello FS2024 DevOps Course!!! ;)))) TEST 2, TEST3 AUTOM.');
+    res.send('Das ist die Livedemo von Erxhan!!');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
